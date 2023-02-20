68f79ec7747889a0479d8bbb95abf12755c82cf4
Ke-rry <ogembokerry30@gmail.com>
2023-02-20 11:29:16 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - prints line
+ * main - prints some text
  *
- * Return: 1 (Success)
+ * Return: 1 (Always)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Kor
-par, 2015-10-19\n", stdout);
-return (1);
-}
+	write(2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		59);
 
+	return (1);
+}
