711850396a169e9453fc1a7923bfa84454bb7215
Ke-rry <ogembokerry30@gmail.com>
2023-02-23 13:11:34 -0800

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,3 +5,12 @@ void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
 int convert_day(int month, int day);
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
